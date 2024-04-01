9a64fd7879d03050a409699853360016ec991496
brandaoplaster <brandaoplaster@gmail.com>
2024-03-31 21:03:33 -0300

    feat: Adding validation job

--- domain/job.go
+++ domain/job.go
@@ -1,13 +1,56 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
 
 type Job struct {
-	ID           string
-	OutputBucket string
-	Status       string
-	Video        *Video
-	Error        string
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           string    `valid:"uuid"`
+	OutputBucket string    `valid:"notnull"`
+	Status       string    `valid:"notnull"`
+	Video        *Video    `valid:"-"`
+	Error        string    `valid:"-"`
+	CreatedAt    time.Time `valid:"-"`
+	UpdatedAt    time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func (job *Job) Validate() error {
+	_, erro := govalidator.ValidateStruct(job)
+
+	if erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (job *Job) prepare() {
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+}
+
+func NewJob(output string, video *Video, status string) (*Job, error) {
+	job := Job{
+		OutputBucket: output,
+		Video:        video,
+		Status:       status,
+	}
+
+	job.prepare()
+
+	error := job.Validate()
+
+	if error != nil {
+		return nil, error
+	}
+
+	return &job, nil
 }
